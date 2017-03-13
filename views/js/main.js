f8247e977e90745d0351a4cc90e69a900be0cf10
eqDeveloper <equin24@yahoo.com>
2017-03-13 15:31:08 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -501,9 +501,13 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
+  // Measure top outside of the loop
+  var top = document.body.scrollTop / 1250;
+  var phase;
+
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(top + i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
